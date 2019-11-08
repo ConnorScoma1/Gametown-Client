a466fc7aebf6b80d76125fe473f53306045dace4
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-11-08 11:45:14 -0600

    creating server file

---
+++ srever.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const path = require('path');
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+    console.log(`sending file from public folder on port ${port}`)
+});
+
+app.listen(port, () => console.log(`App Listening on Port ${port}`))
