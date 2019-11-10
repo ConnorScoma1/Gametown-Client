ee8af513467b3e9cfdcf628278c563dc1dec238e
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-11-10 10:20:10 -0600

    testing server file

--- srever.js
+++ srever.js
@@ -1,18 +1,18 @@
-const express = require('express');
+  
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-const path = require('path');
+var path = require("path");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 /*=======================================
     Setting Up Routes
 =========================================*/
 
 // Main Landing Page Route
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
-    console.log(`sending file from public folder on port ${port}`)
-});
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+  });
 
-app.listen(port, () => console.log(`App Listening on Port ${port}`))
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
