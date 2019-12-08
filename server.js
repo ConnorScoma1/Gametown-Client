f01e8970cc6993e327bb8c4eb9ab890a555c3cdd
ubuntu <39754953+ConnorScoma1@users.noreply.github.com>
2019-12-08 10:44:22 -0600

    Setting Up Backend Routes for About Page

--- server.js
+++ server.js
@@ -15,4 +15,8 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "/public/index.html"));
   });
 
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/about.html"));
+  });
+
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
